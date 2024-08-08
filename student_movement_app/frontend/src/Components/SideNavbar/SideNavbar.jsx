c98b1c48bb0ba4e615c169d0696e628e528243c5
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-08-08 14:26:52 +0100

    update sidenavbar

--- student_movement_app/frontend/src/Components/SideNavbar/SideNavbar.jsx
+++ student_movement_app/frontend/src/Components/SideNavbar/SideNavbar.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SideNavbar.css'
 import dashboard_icon from '../../assets/Images/dashboard-icon.png'
 import student_icon from '../../assets/Images/student-icon.png'
 import notification_icon from '../../assets/Images/notification-icon.png'
 import transfer_icon from '../../assets/Images/transfer-icon.png'
+import menu_icon from '../../assets/Images/menu-icon.png'
+
 
 const SideNavbar = () => {
-  return (
-      <nav className='side-nav'>
-          <ul>
-            <div className='icon-box'><img src={dashboard_icon} alt="" className='icons'/><li >Dashboard</li></div>
-              <div className='icon-box'><img src={student_icon} alt="" className='icons'/> <li>Add Student</li></div>
-             <div className='icon-box'><img src={transfer_icon} alt="" className='icons'/><li>Transfer</li></div>
-              <div className='icon-box'><img src={notification_icon} alt="" className='icons'/><li>Notification</li></div>
-              
+    const [mobileMenu, setMobileMenu] = useState(false)
+    
+    const toggleMenu = () => {
+        setMobileMenu(!mobileMenu)
+    }
+    
 
+
+  return (
+      <nav className={`side-nav ${mobileMenu?'show-sidenav' :''}`}>
+          <img src={menu_icon} alt="" className='side-menu-icon' onClick={toggleMenu} />
+          <ul className={`sidenav-content ${mobileMenu?'show-sidenav' :''}`}>
+            <span className='icon-box'><img src={dashboard_icon} alt="" className='icons'/><li >Dashboard</li></span>
+              <span className='icon-box'><img src={student_icon} alt="" className='icons'/> <li>Add Student</li></span>
+             <span className='icon-box'><img src={transfer_icon} alt="" className='icons'/><li>Transfer</li></span>
+              <span className='icon-box'><img src={notification_icon} alt="" className='icons'/><li>Notification</li></span>
           </ul>
           
     </nav>
