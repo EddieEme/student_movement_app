6521b2168aa5209b0c996ba00913dbd197cdba92
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-08-11 00:59:52 +0100

    update on chart component

--- student_movement_app/frontend/src/Components/Chart/Chart.jsx
+++ student_movement_app/frontend/src/Components/Chart/Chart.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
-import './Chart.css'
+import React from 'react';
+import './Chart.css';
+import { Card, Row, Col, Button, Container } from 'react-bootstrap'
+import { PieChart, Pie, BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const summaryData = [
+  {
+    summaryLabel: 'Total',
+    value: 78000,
+  },
+  {
+    summaryLabel: 'Transfer In',
+    value: 800000,
+  },
+  {
+    summaryLabel: 'Transfer Out',
+    value: 150000,
+  },
+];
+const pieData = [
+  {
+    summaryLabel: 'Transfer In',
+    value: 800000,
+  },
+  {
+    summaryLabel: 'Transfer Out',
+    value: 150000,
+  }];
+
+const yearlyData = [
+  { name: 'Jan', transferredIn: 20, transferredOut: 15 },
+  { name: 'Feb', transferredIn: 25, transferredOut: 20 },
+  { name: 'Mar', transferredIn: 30, transferredOut: 25 },
+];
+
 const Chart = () => {
   return (
-    <div className='chart'>Chart</div>
-  )
-}
+    <div className='chart '>
+      <Row className='m-4 '>
+        {summaryData.map((item, index) => (
+          <Col key={index} md={4} className='mb-3'>
+            <Card className='shadow-lg bg-primary'>
+              <Card.Body>
+                <Card.Title className='fs-2 fw-medium'>{item.summaryLabel}</Card.Title>
+                <Card.Text className='h2'>{item.value.toLocaleString()}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+      <Row className='m-4'>
+        <Col className='flex-1'>
+           <Card className='p-4'>
+          <ResponsiveContainer width="100%" height={400}>
+            <PieChart>
+              <Pie data={pieData}  dataKey="value"
+                  nameKey="summaryLabel"
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={80}
+                  fill="#8884d8"
+                label />
+              <Tooltip/>
+            </PieChart>
+          </ResponsiveContainer>
+        </Card>
+        </Col>
+        <Col className='flex-1'>
+           <Card className='p-4'>
+            <ResponsiveContainer width="100%" height={400}>
+              <BarChart data={yearlyData}>
+                <XAxis dataKey='name' />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="transferredIn" fill="#8884d8" name="Transferred In" />
+                <Bar dataKey="transferredOut" fill="#82ca9d" name="Transferred Out" />
+              </BarChart>
+          </ResponsiveContainer>
+        </Card>
+        </Col>
 
-export default Chart
+
+      </Row>
+    </div>
+  );
+};
+
+export default Chart;
--- student_movement_app/frontend/src/Components/Dashboard/Dashboard.jsx
+++ student_movement_app/frontend/src/Components/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LoginNavbar from '../LoginNavbar/LoginNavbar'
 import './Dashboard.css'
 import SideNavbar from '../SideNavbar/SideNavbar'
@@ -6,11 +6,15 @@ import Chart from '../Chart/Chart'
 
 const Dashboard = () => {
   return (
-      <div className='dashboard'>
-      <LoginNavbar className='login-navbar'/>      
-      <SideNavbar className='sidenav'/>
-      <Chart className='chart'/>
-          </div>
+    <div className='dashboard'>
+      <LoginNavbar className='login-navbar' />
+      <div className='dashboard-content'>
+        <SideNavbar className="sidenav-box" />
+        <div className='main-content'>
+          <Chart className="chart-area"/>
+        </div> 
+      </div>
+    </div>
   )
 }
 
