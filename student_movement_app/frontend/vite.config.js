e1b1a5c71cabfc167aaed47b83678d3dae13c562
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-09-14 23:57:44 +0100

    update base routh

--- student_movement_app/frontend/vite.config.js
+++ student_movement_app/frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/student_movement_project/',
   build: {
     chunkSizeWarningLimit: 1000, // Increase limit (in kB)
   },
+  plugins: [react()],
 })
