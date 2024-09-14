c97f32dc99864d14c677a52554984721bdceeeb6
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-09-15 00:55:44 +0100

    fix deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react"
 import Login from "./Components/Login/Login"
 import Home from "./Components/Home/Home"
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from "./Components/Dashboard/Dashboard"
 import AddStudent from "./Components/AddStudent/AddStudent"
 import Transfer from "./Components/Transfer/Transfer"
