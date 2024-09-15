fffb173de8d02dcf7fe90ba4a394ab62991c09bb
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-09-15 01:41:32 +0100

    updating deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/student_movement_app/frontend/',
-  build: {
-    chunkSizeWarningLimit: 1000, // Increase limit (in kB)
-  },
-  plugins: [react()],
-})
+     base: '/student_movement_app/',
+     plugins: [react()],
+     build: {
+       outDir: 'dist',
+       chunkSizeWarningLimit: 1000,
+     },
+   })
