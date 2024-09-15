c19f32dd4bc06663a7287ae0ecf363ddbc59e127
Tasie Ibiam <tasieibiam28@gmail.com>
2024-09-15 23:10:30 +0100

    Added some modifications in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Use environment variables in your Django settings
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG') == 'True'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
